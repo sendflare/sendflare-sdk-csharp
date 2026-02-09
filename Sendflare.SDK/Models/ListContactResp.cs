5f4293d68166cf099386b6569325233dd5b8dc69
conniemckinney001@gmail.com <conniemckinney001@gmail.com>
2026-02-09 14:06:15 +0800

    fix issue

--- Sendflare.SDK/Models/ListContactResp.cs
+++ Sendflare.SDK/Models/ListContactResp.cs
@@ -6,10 +6,46 @@ namespace Sendflare.SDK.Models
     /// <summary>
     /// Get Contact list response entity
     /// </summary>
-    public class ListContactResp : PaginateResp
+    public class ListContactResp
     {
+        // CommonResponse fields
+        [JsonPropertyName("requestId")]
+        public string RequestId { get; set; }
+
+        [JsonPropertyName("code")]
+        public int Code { get; set; }
+
+        [JsonPropertyName("success")]
+        public bool Success { get; set; }
+
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
+
+        [JsonPropertyName("ts")]
+        public long Ts { get; set; }
+
+        // PaginateResp fields
+        [JsonPropertyName("page")]
+        public int Page { get; set; }
+
+        [JsonPropertyName("pageSize")]
+        public int PageSize { get; set; }
+
+        [JsonPropertyName("totalCount")]
+        public long TotalCount { get; set; }
+
+        // Data wrapper with list
         [JsonPropertyName("data")]
-        public List<Dictionary<string, string>> List { get; set; }
+        public ContactListData Data { get; set; }
+
+        /// <summary>
+        /// Nested data structure containing the contact list
+        /// </summary>
+        public class ContactListData
+        {
+            [JsonPropertyName("list")]
+            public List<Dictionary<string, string>> List { get; set; }
+        }
     }
 }
 
